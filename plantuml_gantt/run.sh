1b1f5d534c42fab6e01043efd43ec73628352d9d
skramm <sebastien.kramm@univ-rouen.fr>
2024-06-15 00:02:43 +0200

    ajout exemples Gantt

--- plantuml_gantt/run.sh
+++ plantuml_gantt/run.sh
@@ -1,9 +1,9 @@
-
+mkdir -p out
 set -x
 for a in *.gantt
 do
-	java -jar /usr/local/bin/plantuml.jar $a
-#	plantuml -tsvg $<
+	java -jar /usr/local/bin/plantuml.jar -tsvg -output out/ $a
+	java -jar /usr/local/bin/plantuml.jar -output out/ $a	
 done
 
 	
