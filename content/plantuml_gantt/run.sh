cfeb56b35255b5684bcd3e9ed108fc3df92133d2
skramm <sebastien.kramm@univ-rouen.fr>
2025-01-11 18:55:15 +0100

    update CI

--- content/plantuml_gantt/run.sh
+++ content/plantuml_gantt/run.sh
@@ -2,8 +2,8 @@ mkdir -p out
 set -x
 for a in *.gantt
 do
-	java -jar /usr/local/bin/plantuml.jar -tsvg -output out/ $a
-	java -jar /usr/local/bin/plantuml.jar -output out/ $a	
+	java -jar /usr/local/bin/plantuml.jar -tsvg -output out2/ $a
+	java -jar /usr/local/bin/plantuml.jar -output out2/ $a	
 done
 
 	
