7603a14f2b88a07ff36de8ec2474f9e7ee28bae7
skramm <sebastien.kramm@univ-rouen.fr>
2025-10-18 16:57:16 +0200

    linux cds: suppression upload artifacts

---
+++ content/commands_csv/get_type.sh
@@ -0,0 +1,30 @@
+set +x
+out=out.csv
+if [ -f $out ]; then rm $out; fi
+
+
+while IFS=";" read -a line
+do
+#	echo -n "line=$line"
+	if [ ${#line} != 0 ]
+	then
+		echo -n "${line[0]};${line[1]};${line[2]};" >> $out
+		type ${line[0]} 1>/tmp/commandtype 2>/dev/null
+		ret=$?
+		if [ $ret != 0 ]
+		then
+			echo "NA" >> $out
+		else
+			grep builtin /tmp/commandtype 1>/dev/null
+			if [ $? = 0 ]
+			then
+				echo "_builtin_" >> $out
+			else
+				echo "_installed_" >> $out
+			fi
+		fi
+	fi
+	
+done < linux_commands.csv
+#done < test.csv
+
